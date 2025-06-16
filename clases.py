bf44b1e019b31e1570d1b97b7259f311bdfc8d18
Diego-2019 <diego190703@hotmail.com>
2025-06-16 15:22:26 -0600

    todo classes

--- clases.py
+++ clases.py
@@ -6,5 +6,16 @@ class MyStruct:
     A: int
     B: int
 
+# todo: Clase Alice que representa al emisor
 class Alice:
-    def __init__(self, curve, p, P, Q):
+    # def __init__(self, curve, p, P, Q):
+    #     self.curve
+
+
+# todo: Clase Bob que representa al receptor
+class Bob:
+    # def __init__(self, curve, p, P, Q):
+
+# todo: Clase ECC que representa el entorno
+class ECC:
+    # def __init__(self, curve, p, P, Q):
