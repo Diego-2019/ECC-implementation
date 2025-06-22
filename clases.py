49f2460b91f1d7650485e51ab41ebe50809841d0
Diego-2019 <diego190703@hotmail.com>
2025-06-22 05:03:30 -0600

    Correccion del archivo clases.py

--- clases.py
+++ clases.py
@@ -18,12 +18,15 @@ class Curve:
         if p <= 3:
             raise ValueError("p must be a prime grater than 3")
         
+        # Prime number
+        self.p = p
+        # least common multiple of aDen and bDen
         self.l = math.lcm(aDen, bDen)
 
         # A = (a / n) * l^4 = a * inverse mod n * l^4
-        self.A = aNum * pow(aDen, -1, p) * pow(self.l, 4, p) % p
+        self.A = aNum * pow(aDen, -1, self.p) * pow(self.l, 4, self.p) % self.p
         # B = (b / m) * l^6 = b * inverse mod m * l^6
-        self.B = bNum * pow(bDen, -1, p) * pow(self.l, 6, p) % p
+        self.B = bNum * pow(bDen, -1, self.p) * pow(self.l, 6, self.p) % self.p
 
     # Method to check if a point is on the curve
     def is_on_curve(self, P: Point) -> bool:
@@ -33,7 +36,7 @@ class Curve:
         
         x, y = P
 
-        return (pow(y, 2) - pow(x, 3) - self.A * x - self.B) % p == 0
+        return (pow(y, 2) - pow(x, 3) - self.A * x - self.B) % self.p == 0
     
     # Additive inverse method of P
     def negate(self, P: Point) -> Point:
@@ -42,7 +45,7 @@ class Curve:
         
         x, y = P
 
-        return (x, -y % p)
+        return (x, -y % self.p)
     
     # Point addition method
     def point_add(self, P: Point, Q: Point) -> Point:
@@ -56,18 +59,18 @@ class Curve:
         x2, y2 = Q
 
         # Case when P + (-P) = 0 where Q = -P 
-        if x1 == x2 and (y1 + y2) % p == 0:
+        if x1 == x2 and (y1 + y2) % self.p == 0:
             return None
         
         # Addition when P = Q (point doubling)
         if P == Q:
-            m = (3 * pow(x1, 2) + self.A) * pow(2 * y1, -1, p) % p
+            m = (3 * pow(x1, 2) + self.A) * pow(2 * y1, -1, self.p) % self.p
         # Addition when P != Q (point addition)
         else:
-            m = (y2 - y1) * pow(x2 - x1, -1, p) % p
+            m = (y2 - y1) * pow(x2 - x1, -1, self.p) % self.p
 
-        x3 = (pow(m, 2) - x1 - x2) % p
-        y3 = (m * (x1 - x3) - y1) % p
+        x3 = (pow(m, 2) - x1 - x2) % self.p
+        y3 = (m * (x1 - x3) - y1) % self.p
 
         return (x3, y3)
     
@@ -99,17 +102,17 @@ class Curve:
         if P is None:
             return None
         x, y = P
-        X = (x * pow(self.l, 2, p)) % p
-        Y = (y * pow(self.l, 3, p)) % p
+        X = (x * pow(self.l, 2, self.p)) % self.p
+        Y = (y * pow(self.l, 3, self.p)) % self.p
         return (X, Y)
 
     def from_fractional(self, P: Point) -> Point:
         if P is None:
             return None
         X, Y = P
-        linv = pow(self.l, -1, p)
-        x = (X * pow(linv, 2, p)) % p
-        y = (Y * pow(linv, 3, p)) % p
+        linv = pow(self.l, -1, self.p)
+        x = (X * pow(linv, 2, self.p)) % self.p
+        y = (Y * pow(linv, 3, self.p)) % self.p
         return (x, y)
 
 # Class for Alice and Bob
@@ -158,23 +161,3 @@ def decrypt(curve: Curve, P: Point, n: int, d: int, C: Tuple[Point, Point]) -> P
     M = curve.point_add(C2, S_neg)
 
     return M
-
-
-
-
-
-
-
-# ?Seccion para probar los metodos
-aNum, aDen, bNum, bDen = 7, 3, 2, 5
-p = 178987
-l = 15
-P = (116485, 32401)
-C = Curve(p, aNum, aDen, bNum, bDen)
-
-# print((bNum * pow(bDen, -1, p)) * pow(l, 6, p) % p)
-# print(C.is_on_curve(P))
-# print(C.negate(P))
-# print(C.point_add((5,2), (2,7)))
-# print(C.order(P))
-# assert C.is_on_curve(P)
